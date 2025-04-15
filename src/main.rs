1e2a228f11b2692e5e48911c1a38d48078fdfd1e
skearya <77034153+skearya@users.noreply.github.com>
2025-04-15 14:51:43 -0400

    lower chances of overflow

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use image::{Rgb, RgbImage};
 use rand::{
     Rng,
-    distr::{Distribution, StandardUniform, Uniform, weighted::WeightedIndex},
-    rng,
+    distr::{Distribution, Uniform, weighted::WeightedIndex},
 };
 
 #[derive(Debug)]
@@ -23,12 +22,16 @@ enum Operation {
     Cos(Expr),
 }
 
-impl Distribution<Expr> for StandardUniform {
-    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Expr {
-        let expr_weights = [1, 1, 1, 5];
+impl Expr {
+    fn random<R: Rng + ?Sized>(rng: &mut R, depth: u32) -> Expr {
+        // Chances that we choose to do an operation and keep recursing
+        // Decreases as depth increases to lower chances of stack overflow
+        let op_expr_weight = (1.0 / (depth as f32 + 1.0)) * 10.0;
+
+        let expr_weights = [1.0, 1.0, 1.0, op_expr_weight];
         let expr_dist = WeightedIndex::new(expr_weights).unwrap();
 
-        let op_weights = [1, 1, 1, 1, 1, 1];
+        let op_weights = [1.0, 1.0, 1.0, 1.0, 1.0, 1.0];
         let op_dist = WeightedIndex::new(op_weights).unwrap();
 
         match expr_dist.sample(rng) {
@@ -36,12 +39,12 @@ impl Distribution<Expr> for StandardUniform {
             1 => Expr::Y,
             2 => Expr::Literal(Uniform::new(1.0, 255.0).unwrap().sample(rng)),
             3 => Expr::Operation(Box::new(match op_dist.sample(rng) {
-                0 => Operation::Add(rng.random(), rng.random()),
-                1 => Operation::Sub(rng.random(), rng.random()),
-                2 => Operation::Mul(rng.random(), rng.random()),
-                3 => Operation::Div(rng.random(), rng.random()),
-                4 => Operation::Sin(rng.random()),
-                5 => Operation::Cos(rng.random()),
+                0 => Operation::Add(Expr::random(rng, depth + 1), Expr::random(rng, depth + 1)),
+                1 => Operation::Sub(Expr::random(rng, depth + 1), Expr::random(rng, depth + 1)),
+                2 => Operation::Mul(Expr::random(rng, depth + 1), Expr::random(rng, depth + 1)),
+                3 => Operation::Div(Expr::random(rng, depth + 1), Expr::random(rng, depth + 1)),
+                4 => Operation::Sin(Expr::random(rng, depth + 1)),
+                5 => Operation::Cos(Expr::random(rng, depth + 1)),
                 _ => unreachable!(),
             })),
             _ => unreachable!(),
@@ -52,13 +55,19 @@ impl Distribution<Expr> for StandardUniform {
 fn main() {
     let mut img = RgbImage::new(512, 512);
 
-    let expr = (
-        Expr::Operation(P(Operation::Mul(Expr::X, Expr::Literal(255.0)))),
-        Expr::Operation(P(Operation::Mul(Expr::Y, Expr::Literal(255.0)))),
-        Expr::Literal(0.0),
-    );
+    // let expr = (
+    //     Expr::Operation(P(Operation::Mul(Expr::X, Expr::Literal(255.0)))),
+    //     Expr::Operation(P(Operation::Mul(Expr::Y, Expr::Literal(255.0)))),
+    //     Expr::Literal(0.0),
+    // );
 
-    let expr: (Expr, Expr, Expr) = (rng().random(), rng().random(), rng().random());
+    let mut rng = rand::rng();
+
+    let expr: (Expr, Expr, Expr) = (
+        Expr::random(&mut rng, 0),
+        Expr::random(&mut rng, 0),
+        Expr::random(&mut rng, 0),
+    );
     dbg!(&expr);
 
     for (x, y, Rgb(color)) in img.enumerate_pixels_mut() {
