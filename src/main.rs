4033f74e94ba9ea862c87ae541940ea6251bea2a
skearya <77034153+skearya@users.noreply.github.com>
2025-04-17 20:48:49 -0400

    more ops

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use std::f32::consts::PI;
 use std::fmt::Display;
+use std::ops::{Add, Div, Mul, Sub};
 
 use glam::Vec2;
 use image::{Rgb, RgbImage};
@@ -28,18 +30,28 @@ enum Operation {
     Log(Expr, Expr),
     Sin(Expr),
     Cos(Expr),
+    Tan(Expr),
+    ASin(Expr),
+    ACos(Expr),
+    ATan(Expr),
+    Radians(Expr),
+    Degrees(Expr),
+    Sqrt(Expr),
+    Fract(Expr),
 }
 
 impl Expr {
     fn random<R: Rng + ?Sized>(rng: &mut R, depth: u32) -> Expr {
         // Chances that we choose to do an operation and keep recursing
         // Decreases as depth increases to lower chances of stack overflow
-        let op_expr_weight = (1.0 / (depth as f32 + 1.0)) * 10.0;
+        let op_expr_weight = (1.0 / (depth as f32 + 1.0)) * 25.0;
 
         let expr_weights = [1.0, 1.0, 1.0, op_expr_weight];
         let expr_dist = WeightedIndex::new(expr_weights).unwrap();
 
-        let op_weights = [1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0];
+        let op_weights = [
+            1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0,
+        ];
         let op_dist = WeightedIndex::new(op_weights).unwrap();
 
         match expr_dist.sample(rng) {
@@ -56,6 +68,14 @@ impl Expr {
                 6 => Operation::Log(Expr::random(rng, depth + 1), Expr::random(rng, depth + 1)),
                 7 => Operation::Sin(Expr::random(rng, depth + 1)),
                 8 => Operation::Cos(Expr::random(rng, depth + 1)),
+                9 => Operation::Tan(Expr::random(rng, depth + 1)),
+                10 => Operation::ASin(Expr::random(rng, depth + 1)),
+                11 => Operation::ACos(Expr::random(rng, depth + 1)),
+                12 => Operation::ATan(Expr::random(rng, depth + 1)),
+                13 => Operation::Radians(Expr::random(rng, depth + 1)),
+                14 => Operation::Degrees(Expr::random(rng, depth + 1)),
+                15 => Operation::Sqrt(Expr::random(rng, depth + 1)),
+                16 => Operation::Fract(Expr::random(rng, depth + 1)),
                 _ => unreachable!(),
             })),
             _ => unreachable!(),
@@ -78,6 +98,14 @@ fn eval(expr: &Expr, x: f32, y: f32) -> f32 {
             Operation::Log(left, right) => eval(right, x, y).log(eval(left, x, y)),
             Operation::Sin(left) => eval(left, x, y).sin(),
             Operation::Cos(left) => eval(left, x, y).cos(),
+            Operation::Tan(left) => eval(left, x, y).tan(),
+            Operation::ASin(left) => eval(left, x, y).asin(),
+            Operation::ACos(left) => eval(left, x, y).acos(),
+            Operation::ATan(left) => eval(left, x, y).atan(),
+            Operation::Radians(left) => PI * eval(left, x, y) / 180.0,
+            Operation::Degrees(left) => 180.0 * eval(left, x, y) / PI,
+            Operation::Sqrt(left) => eval(left, x, y).sqrt(),
+            Operation::Fract(left) => eval(left, x, y) % 1.0,
         },
     }
 }
@@ -98,10 +126,34 @@ impl Display for Expr {
                 Operation::Log(left, right) => write!(f, "log(base {})({})", left, right),
                 Operation::Sin(left) => write!(f, "sin({})", left),
                 Operation::Cos(left) => write!(f, "cos({})", left),
+                Operation::Tan(left) => write!(f, "tan({})", left),
+                Operation::ASin(left) => write!(f, "asin({})", left),
+                Operation::ACos(left) => write!(f, "acos({})", left),
+                Operation::ATan(left) => write!(f, "atan({})", left),
+                Operation::Radians(left) => write!(f, "radians({})", left),
+                Operation::Degrees(left) => write!(f, "degrees({})", left),
+                Operation::Sqrt(left) => write!(f, "sqrt({})", left),
+                Operation::Fract(left) => write!(f, "fract({})", left),
             },
         }
     }
 }
+
+// trait Vec2Utils {
+//     fn sorted(x: f32, y: f32) -> Vec2;
+//     fn preform(left: Self, right: Self, f: impl Fn(f32, f32) -> f32) -> Vec2;
+// }
+
+// impl Vec2Utils for Vec2 {
+//     fn sorted(x: f32, y: f32) -> Vec2 {
+//         Vec2::new(x.min(y), x.max(y))
+//     }
+
+//     // Preform an operation with both sides of 2 Vec2's and ensure the output is sorted
+//     fn preform(left: Vec2, right: Vec2, f: impl Fn(f32, f32) -> f32) -> Vec2 {
+//         Vec2::sorted(f(left.x, right.x), f(left.y, right.y))
+//     }
+// }
 
 // TODO: Revisit tracking the specific range of an expression
 // fn range(expr: &Expr) -> Vec2 {
@@ -110,25 +162,19 @@ impl Display for Expr {
 //         Expr::Y => Vec2::new(0.0, 1.0),
 //         Expr::Literal(x) => Vec2::new(*x, *x),
 //         Expr::Operation(operation) => match operation.as_ref() {
-//             Operation::Add(left, right) => range(left) + range(right),
-//             Operation::Sub(left, right) => range(left) - range(right),
-//             Operation::Mul(left, right) => range(left) * range(right),
-//             Operation::Div(left, right) => range(left) / range(right),
-//             Operation::Mod(_, right) => range(right).with_x(0.0),
-//             Operation::Pow(left, right) => {
-//                 let left = range(left);
-//                 let right = range(right);
-
-//                 Vec2::new(left.x.powf(right.x), left.y.powf(right.y))
-//             }
-//             Operation::Log(left, right) => {
-//                 let left = range(left);
-//                 let right = range(right);
-
-//                 Vec2::new(right.x.log(left.x), right.y.log(left.y))
-//             }
+//             Operation::Add(left, right) => Vec2::preform(range(left), range(right), f32::add),
+//             Operation::Sub(left, right) => Vec2::preform(range(left), range(right), f32::sub),
+//             Operation::Mul(left, right) => Vec2::preform(range(left), range(right), f32::mul),
+//             Operation::Div(left, right) => Vec2::preform(range(left), range(right), f32::div),
+//             Operation::Mod(_left, right) => Vec2::sorted(0.0, range(right).y),
+//             Operation::Pow(left, right) => Vec2::preform(range(left), range(right), f32::powf),
+//             Operation::Log(left, right) => Vec2::preform(range(right), range(left), f32::log),
 //             Operation::Sin(left) => range(left).map(f32::sin),
 //             Operation::Cos(left) => range(left).map(f32::cos),
+//             Operation::Tan(left) => range(left).map(f32::tan),
+//             Operation::ASin(left) => range(left).map(f32::atan),
+//             Operation::ACos(left) => range(left).map(f32::acos),
+//             Operation::ATan(left) => range(left).map(f32::atan),
 //         },
 //     }
 // }
@@ -153,6 +199,7 @@ fn range(expr: &Expr) -> Vec2 {
     Vec2::new(min, max)
 }
 
+// TODO: Something in here keeps being infinity
 fn fix(expr: Expr) -> Expr {
     let range = range(&expr);
 
@@ -173,12 +220,12 @@ fn main() {
     let mut img = RgbImage::new(WIDTH, HEIGHT);
 
     // let expr = (
-    //     Expr::Operation(P(Operation::Mul(Expr::X, Expr::Literal(2042.0)))),
+    //     Expr::Operation(P(Operation::Mul(Expr::X, Expr::Literal(255.0)))),
     //     Expr::Operation(P(Operation::Mul(Expr::Y, Expr::Literal(255.0)))),
     //     Expr::Literal(0.0),
     // );
 
-    // let mut rng = Seeder::from("Waow").into_rng::<ChaCha8Rng>();
+    // let mut rng = Seeder::from("ee").into_rng::<ChaCha8Rng>();
 
     let mut rng = rand::rng();
 
@@ -195,8 +242,6 @@ fn main() {
     println!("green: {}", expr.1);
     println!("blue: {}", expr.2);
 
-    let mut count = 0;
-
     for (x, y, Rgb(color)) in img.enumerate_pixels_mut() {
         // 0.0 - 1.0 value coords
         let u = x as f32 / WIDTH as f32;
@@ -210,26 +255,12 @@ fn main() {
         // let u = x as f32 / 512.0;
         // let v = 1.0 - y as f32 / 512.0;
 
-        if eval(&expr.0, u, v) >= 255.0 {
-            count += 1;
-        }
-
-        if eval(&expr.1, u, v) >= 255.0 {
-            count += 1;
-        }
-
-        if eval(&expr.2, u, v) >= 255.0 {
-            count += 1;
-        }
-
         *color = [
             eval(&expr.0, u, v) as u8,
             eval(&expr.1, u, v) as u8,
             eval(&expr.2, u, v) as u8,
         ];
     }
-
-    dbg!(count);
 
     img.save("out.png").unwrap();
 }
